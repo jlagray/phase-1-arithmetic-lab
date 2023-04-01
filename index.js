141fe4df9cf8437b552341c2b2f42e85516dda5e
Jamie Grayson <126696913+jlagray@users.noreply.github.com>
2023-04-01 18:04:47 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() > 0);
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [5, 20, 15, 10];
+const max = Math.max(...numbers);
